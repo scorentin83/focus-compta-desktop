fb00b564fec29c074a405c6efb70a9337b8507a0
scorentin83 <scorentin83@gmail.com>
2026-06-08 23:46:34 +0200

    Debug companies schema

--- backend/server.js
+++ backend/server.js
@@ -47,10 +47,10 @@ function ensureColumn(table, column, definition) {
   }
 }
 
-ensureColumn("companies", "siren", "TEXT");
-ensureColumn("companies", "vat_regime", "TEXT DEFAULT 'normal'");
-ensureColumn("companies", "fiscal_year_end", "TEXT");
 ensureColumn("companies", "notes", "TEXT");
+ensureColumn("companies", "siren", "TEXT");
+ensureColumn("companies", "vat_regime", "TEXT");
+ensureColumn("companies", "fiscal_year_end", "TEXT");
 ensureColumn("companies", "updated_at", "DATETIME");
 
 console.log(
