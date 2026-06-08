bdee8ee5a5bf890557288a03861c012f8951bb9d
scorentin83 <scorentin83@gmail.com>
2026-06-08 23:12:28 +0200

    Test API companies

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -1,1 +1,18 @@
 const API_URL = "/api";
+
+async function loadCompanies() {
+  try {
+    const response = await fetch(`${API_URL}/companies`);
+    const companies = await response.json();
+
+    console.log("Sociétés :", companies);
+  } catch (error) {
+    console.error("Erreur API :", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Focus Compta Web v0.87 démarré");
+
+  loadCompanies();
+});
